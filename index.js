26bc8ce12ec481f1bd5385309420d26405ca5154
Lucas Dobson <lucasdobson88@gmail.com>
2018-05-12 13:18:31 +1000

    Added passport and passport-google-oauth20 to the project for authorisation with google

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken)
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
