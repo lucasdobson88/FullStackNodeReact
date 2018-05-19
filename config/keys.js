8829ec3b7673d24f704a57eda1ffa942b204ff73
Lucas Dobson <lucasdobson88@gmail.com>
2018-05-19 11:49:34 +1000

    Added cookies, finalised authentication process, added in environment credential handling

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+//keys.js - figure out what credentials to return
+
+if(process.env.NODE_ENV === 'production'){
+  //we are in production - return production keys
+  module.exports = require('./prod');
+}
+else{
+  //we are in development - return dev keys
+  module.exports = require('./dev');
+}
