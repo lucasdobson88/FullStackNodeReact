d94f2af65243e85e7a29c3dfd0d2d04416068604
Lucas Dobson <lucasdobson88@gmail.com>
2018-06-05 09:44:57 +1000

    Added redux to project, initialised store, refactored functions to use async await syntax

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,14 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback',
     proxy: true
   },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOne({googleId: profile.id })
-    .then((existingUser) => {
-      if(existingUser){
-        // we already have a user
-        done(null, existingUser);
-      }
-      else{
-        //need to create a new user
-        new User({googleId: profile.id})
-        .save()
-        .then(user => done(null, user));
-      }
-    })
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({googleId: profile.id });
+    if(existingUser){
+      // we already have a user
+      return done(null, existingUser);
+    }
+    //need to create a new user
+    const user = await new User({googleId: profile.id}).save()
+    done(null, user);
   })
 );
