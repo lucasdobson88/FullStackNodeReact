c71ec4661c4fef404547970c6db91b3a78a70051
Lucas Dobson <lucasdobson88@gmail.com>
2018-06-05 22:09:38 +1000

    Added header to application

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 
-const Header = () => <h2>Header</h2>
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
@@ -11,15 +11,15 @@ const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
-          <Route />
+          <Header />
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/surveys" component={Dashboard}/>
+          <Route path="/surveys/new" component={SurveyNew}/>
         </div>
       </BrowserRouter>
-      <Header />
-      <Dashboard />
-      <SurveyNew />
     </div>
   );
 };
