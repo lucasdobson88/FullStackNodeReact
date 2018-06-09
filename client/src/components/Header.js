c7cb4003e66a264408b33eb86a2a47cb0715bb1e
Lucas Dobson <lucasdobson88@gmail.com>
2018-06-09 12:50:32 +1000

    Finalised authentication functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
+import {connect}  from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+
+  renderContent() {
+    switch(this.props.auth){
+      case null:
+        return;
+      case false:
+       return (
+            <li><a href="/auth/google">Login with Google</a></li>
+       );
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
   render(){
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+           to={this.props.auth ? "/surveys" : "/"}
+           className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">
-            <li>
-              <a href="/">Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -18,4 +37,8 @@ class Header extends Component {
   };
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,9 @@
-export default function(state = {}, action){
+import * as types from '../actions/types';
+
+export default function(state = null, action){
   switch(action.type){
+    case types.FETCH_USER:
+    return action.payload || false;
     default:
     return state;
   }
