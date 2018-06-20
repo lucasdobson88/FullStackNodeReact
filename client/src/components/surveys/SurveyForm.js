db7c2d11bec5368dcb7aa3272c5a38bae0567bf4
Lucas Dobson <lucas.dobson@stratton.com.au>
2018-06-20 15:43:30 +1000

    Added in redux form,

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,23 @@
+// SurveyForm shows a form for a user to add input
+import React, {Component} from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+class SurveyForm extends Component{
+  render(){
+    return (
+        <div>
+          <form
+            onSubmit={this.props.handleSubmit(values => console.log(values))}
+          >
+            <Field type="text" name="surveyTitle" component="input" placeholder="Title" />
+            <Field type="text" name="surveySubject" component="input" placeholder="Title" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyForm);
