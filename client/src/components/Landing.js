d061961a665ce756b987627095d4fe38d7dc95d9
Lucas Dobson <lucas.dobson@stratton.com.au>
2018-08-06 14:17:06 +1000

    test1

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -10,7 +10,7 @@ const Landing = () => {
       <p>Collect feedback from your users.</p>
       <p>Please login to use our services.</p>
       <div>
-       <iframe class="stratton-iframe" src="https://www.strattonfinance.com.au/itpcarloans?rcid=81b905be-6148-4640-9fdd-a904011ad209">
+       <iframe class="stratton-iframe" src="https://www.strattonfinance.com.au/test-iframe?rcid=81b905be-6148-4640-9fdd-a904011ad209">
 </iframe>
 
   <iframe class="stratton-iframe" src="https://en.wikipedia.org/wiki/Main_Page">
